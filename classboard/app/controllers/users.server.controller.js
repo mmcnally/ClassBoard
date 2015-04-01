f73d867e919dd50af45ada37c9f306072a271207
Matt McNally <mgmcnally@umass.edu>
2015-04-01 18:15:44 -0400

    moved user controlled into one file instead of many fragmented ones

--- classboard/app/controllers/users.server.controller.js
+++ classboard/app/controllers/users.server.controller.js
@@ -3,14 +3,260 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash');
+var _ = require('lodash'),
+	errorHandler = require('../errors.server.controller'),
+	mongoose = require('mongoose'),
+	passport = require('passport'),
+	User = mongoose.model('User');
+
+var _ = require('lodash'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User');
+
+/*********************************************************************************
+ *	Profile Stuff
+ *********************************************************************************/
 
 /**
- * Extend user's controller
+ * Update user details
  */
-module.exports = _.extend(
-	require('./users/users.authentication.server.controller'),
-	require('./users/users.authorization.server.controller'),
-	require('./users/users.password.server.controller'),
-	require('./users/users.profile.server.controller')
-);
+exports.update = function(req, res) {
+	// Init Variables
+	var user = req.user;
+	var message = null;
+
+	// For security measurement we remove the roles from the req.body object
+	delete req.body.roles;
+
+	if (user) {
+		// Merge existing user
+		user = _.extend(user, req.body);
+		user.updated = Date.now();
+		user.displayName = user.firstName + ' ' + user.lastName;
+
+		user.save(function(err) {
+			if (err) {
+				return res.status(400).send({
+					message: errorHandler.getErrorMessage(err)
+				});
+			} else {
+				req.login(user, function(err) {
+					if (err) {
+						res.status(400).send(err);
+					} else {
+						res.json(user);
+					}
+				});
+			}
+		});
+	} else {
+		res.status(400).send({
+			message: 'User is not signed in'
+		});
+	}
+};
+
+/**
+ * Send User
+ */
+exports.me = function(req, res) {
+	res.json(req.user || null);
+};
+
+
+
+/******************************************************************************************
+ * Password Stuff
+ ******************************************************************************************/
+
+
+User = mongoose.model('User');
+
+/**
+ * Change Password
+ */
+exports.changePassword = function(req, res) {
+	// Init Variables
+	var passwordDetails = req.body;
+
+	if (req.user) {
+		if (passwordDetails.newPassword) {
+			User.findById(req.user.id, function(err, user) {
+				if (!err && user) {
+					if (user.authenticate(passwordDetails.currentPassword)) {
+						if (passwordDetails.newPassword === passwordDetails.verifyPassword) {
+							user.password = passwordDetails.newPassword;
+
+							user.save(function(err) {
+								if (err) {
+									return res.status(400).send({
+										message: errorHandler.getErrorMessage(err)
+									});
+								} else {
+									req.login(user, function(err) {
+										if (err) {
+											res.status(400).send(err);
+										} else {
+											res.send({
+												message: 'Password changed successfully'
+											});
+										}
+									});
+								}
+							});
+						} else {
+							res.status(400).send({
+								message: 'Passwords do not match'
+							});
+						}
+					} else {
+						res.status(400).send({
+							message: 'Current password is incorrect'
+						});
+					}
+				} else {
+					res.status(400).send({
+						message: 'User is not found'
+					});
+				}
+			});
+		} else {
+			res.status(400).send({
+				message: 'Please provide a new password'
+			});
+		}
+	} else {
+		res.status(400).send({
+			message: 'User is not signed in'
+		});
+	}
+};
+
+
+
+
+
+/******************************************************************************************
+ * Authorization Stuff
+ ******************************************************************************************/
+
+
+
+/**
+ * User middleware
+ */
+exports.userByID = function(req, res, next, id) {
+	User.findById(id).exec(function(err, user) {
+		if (err) return next(err);
+		if (!user) return next(new Error('Failed to load User ' + id));
+		req.profile = user;
+		next();
+	});
+};
+
+/**
+ * Require login routing middleware
+ */
+exports.requiresLogin = function(req, res, next) {
+	if (!req.isAuthenticated()) {
+		return res.status(401).send({
+			message: 'User is not logged in'
+		});
+	}
+
+	next();
+};
+
+/**
+ * User authorizations routing middleware
+ */
+exports.hasAuthorization = function(roles) {
+	var _this = this;
+
+	return function(req, res, next) {
+		_this.requiresLogin(req, res, function() {
+			if (_.intersection(req.user.roles, roles).length) {
+				return next();
+			} else {
+				return res.status(403).send({
+					message: 'User is not authorized'
+				});
+			}
+		});
+	};
+};
+
+
+/******************************************************************************************
+ * Authentication Stuff
+ ******************************************************************************************/
+
+
+
+/**
+ * Signup
+ */
+exports.signup = function(req, res) {
+	// For security measurement we remove the roles from the req.body object
+	delete req.body.roles;
+
+	// Init Variables
+	var user = new User(req.body);
+	var message = null;
+
+	// Add missing user fields
+	user.displayName = user.firstName + ' ' + user.lastName;
+
+	// Then save the user 
+	user.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			// Remove sensitive data before login
+			user.password = undefined;
+			user.salt = undefined;
+
+			req.login(user, function(err) {
+				if (err) {
+					res.status(400).send(err);
+				} else {
+					res.json(user);
+				}
+			});
+		}
+	});
+};
+
+/**
+ * Signin after passport authentication
+ */
+exports.signin = function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err || !user) {
+			res.status(400).send(info);
+		} else {
+			// Remove sensitive data before login
+			user.password = undefined;
+			user.salt = undefined;
+
+			req.login(user, function(err) {
+				if (err) {
+					console.log('ERROR ' + err);
+					res.status(400).send(err);
+				} else {
+					res.json(user);
+				}
+			});
+		}
+	})(req, res, next);
+};
+
+/**
+ * Signout
+ */
+exports.signout = function(req, res) {
+	req.logout();
+	res.redirect('/');
+};
