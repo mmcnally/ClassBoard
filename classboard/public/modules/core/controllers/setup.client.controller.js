cf26f2acf260cd5285bf79e108965e049db1bf7f
Matt McNally <mgmcnally@umass.edu>
2015-04-14 19:18:22 -0400

    made signUpClass method, created scope variables for forms in view

--- classboard/public/modules/core/controllers/setup.client.controller.js
+++ classboard/public/modules/core/controllers/setup.client.controller.js
@@ -1,13 +1,30 @@
 'use strict';
 
 
-angular.module('core').controller('SetupController', ['$scope', 'Authentication', '$location',
-	function($scope, Authentication, $location) {
+angular.module('core').controller('SetupController', ['$scope', '$http', 'Authentication', '$location',
+	function($scope, $http, Authentication, $location) {
 		$scope.authentication = Authentication;
 		
     // send user to signin if not logged in
 		if (!Authentication.user) {
 				$location.path('/signin');
 		}
+		
+		
+		$scope.SignUp = {};
+		$scope.CreateClass = {admins: [Authentication.user._id], students: []};
+	
+	
+		$scope.signUpClass = function() {
+			$http.post('/course/signUpClass', $scope.CreateClass).success(function(response) {
+				// If successful we assign the response to the global user model
+				$scope.authentication.user = response;
+
+				// And redirect to the index page
+				$location.path('/');
+			}).error(function(response) {
+				$scope.SignUp.error = response.message;
+			});
+		};
 	}
 ]);
