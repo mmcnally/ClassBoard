adad946d89bd1df57698e42fe9ab2ba3f48d8359
agora94 <agora@umass.edu>
2015-04-14 19:45:03 -0400

    users.server.controller brought forgot password function back

--- classboard/app/controllers/users.server.controller.js
+++ classboard/app/controllers/users.server.controller.js
@@ -9,6 +9,10 @@ var _ = require('lodash'),
 	passport = require('passport'),
 	User = mongoose.model('User');
 
+var _ = require('lodash'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User');
+
 /*********************************************************************************
  *	Profile Stuff
  *********************************************************************************/
@@ -28,7 +32,7 @@ exports.update = function(req, res) {
 		// Merge existing user
 		user = _.extend(user, req.body);
 		user.updated = Date.now();
-		// user.displayName = user.firstName + ' ' + user.lastName;
+		user.displayName = user.firstName + ' ' + user.lastName;
 
 		user.save(function(err) {
 			if (err) {
@@ -64,6 +68,83 @@ exports.me = function(req, res) {
 /******************************************************************************************
  * Password Stuff
  ******************************************************************************************/
+
+
+User = mongoose.model('User');
+
+/**
+ * Forgot for reset password (forgot POST)
+ */
+exports.forgot = function(req, res, next) {
+	async.waterfall([
+		// Generate random token
+		function(done) {
+			crypto.randomBytes(20, function(err, buffer) {
+				var token = buffer.toString('hex');
+				done(err, token);
+			});
+		},
+		// Lookup user by username
+		function(token, done) {
+			if (req.body.username) {
+				User.findOne({
+					username: req.body.username
+				}, '-salt -password', function(err, user) {
+					if (!user) {
+						return res.status(400).send({
+							message: 'No account with that username has been found'
+						});
+					} else if (user.provider !== 'local') {
+						return res.status(400).send({
+							message: 'It seems like you signed up using your ' + user.provider + ' account'
+						});
+					} else {
+						user.resetPasswordToken = token;
+						user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+						user.save(function(err) {
+							done(err, token, user);
+						});
+					}
+				});
+			} else {
+				return res.status(400).send({
+					message: 'Username field must not be blank'
+				});
+			}
+		},
+		function(token, user, done) {
+			res.render('templates/reset-password-email', {
+				name: user.displayName,
+				appName: config.app.title,
+				url: 'http://' + req.headers.host + '/auth/reset/' + token
+			}, function(err, emailHTML) {
+				done(err, emailHTML, user);
+			});
+		},
+		// If valid email, send reset email using service
+		function(emailHTML, user, done) {
+			var smtpTransport = nodemailer.createTransport(config.mailer.options);
+			var mailOptions = {
+				to: user.email,
+				from: config.mailer.from,
+				subject: 'Password Reset',
+				html: emailHTML
+			};
+			smtpTransport.sendMail(mailOptions, function(err) {
+				if (!err) {
+					res.send({
+						message: 'An email has been sent to ' + user.email + ' with further instructions.'
+					});
+				}
+
+				done(err);
+			});
+		}
+	], function(err) {
+		if (err) return next(err);
+	});
+};
 
 
 /**
@@ -179,11 +260,6 @@ exports.hasAuthorization = function(roles) {
 		});
 	};
 };
-
-
-
-
-
 
 
 /******************************************************************************************
