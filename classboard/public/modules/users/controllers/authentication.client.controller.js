701c96e6c4a43bb73ec3a76d009fa5cf33e3a4ee
Matt McNally <mgmcnally@umass.edu>
2015-04-14 16:18:22 -0400

    fixed double form field entering

--- classboard/public/modules/users/controllers/authentication.client.controller.js
+++ classboard/public/modules/users/controllers/authentication.client.controller.js
@@ -3,12 +3,15 @@
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
 		$scope.authentication = Authentication;
-
+				
 		// If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/');
 
+		$scope.SignIn = {};
+		$scope.SignUp = {};
+
 		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
+			$http.post('/auth/signup', $scope.SignUp).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
@@ -20,7 +23,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		};
 
 		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
+			$http.post('/auth/signin', $scope.SignIn).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
