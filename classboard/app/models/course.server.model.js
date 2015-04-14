dd1177b1ad66d5c2b2736d3aa1fecd31b06eaa34
Matt McNally <mgmcnally@umass.edu>
2015-04-14 19:15:06 -0400

    changed class schemac to course schema (class is reserved word)

---
+++ classboard/app/models/course.server.model.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+	
+
+
+/**
+ * A Validation function for local strategy properties
+ */
+var validateNonempty = function(property) {
+	if(property) {
+		return (property.length);
+	}
+	return false;	
+};
+
+/**
+ * ClassRoom Schema
+ */
+var CourseSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+    validate: [validateNonempty, 'Please fill in the class name']
+  },
+  admins: {
+    type: [Schema.ObjectId],
+    ref: 'User',
+    required: true,
+    validate: [validateNonempty, 'Please fill in the teachers']
+  },
+  students: {
+    type: [Schema.ObjectId],
+    ref: 'User',
+    required: true,
+    validate: [validateNonempty, 'Please fill in students']
+  },
+	code: {
+		type: Number,
+		required: true,
+		validate: [validateNonempty, 'retreat immediately, hell froze over']
+	},
+  capacity: {
+    type: Number
+  },
+  widgets: {
+    type: [String],
+    default: ['Attendance', 'Quiz', 'Confused']
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+mongoose.model('Course', CourseSchema);
