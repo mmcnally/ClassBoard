23eeb4a94038561ac4c3c1dda4dd69466d878a88
Matt McNally <mgmcnally@umass.edu>
2015-04-13 20:53:45 -0400

    made unsigned in user default to signin/signup view

--- classboard/app/routes/core.server.routes.js
+++ classboard/app/routes/core.server.routes.js
@@ -3,5 +3,7 @@
 module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
+	//var users = require('../../app/controllers/users.server.controller');
 	app.route('/').get(core.index);
+	// app.route('/').get(users.signup);
 };
