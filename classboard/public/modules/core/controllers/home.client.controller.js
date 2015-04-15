59a56a053c04cde3e05e0a9f6416e8e83713a0e3
Michael Lankenau <mlankena@umass.edu>
2015-04-14 21:38:40 -0400

    fixed merge conflict

--- classboard/public/modules/core/controllers/home.client.controller.js
+++ classboard/public/modules/core/controllers/home.client.controller.js
@@ -7,14 +7,9 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
 		if (!Authentication.user) {
 			$location.path('/signin');
-<<<<<<< HEAD
-		}/*
-		else if (Authentication.user.classes.length == 0) {
-=======
 		}
 		else if (Authentication.user.classes && Authentication.user.classes.length === 0) {
->>>>>>> 3be727a399389f3cb84ff662e50f308b2f68cc0c
 			$location.path('/setup');
-		}*/
+		}
 	}
 ]);
