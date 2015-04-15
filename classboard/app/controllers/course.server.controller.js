3835cd081bcf4da2166e88337b7f762611cc9c9b
Matt McNally <mgmcnally@umass.edu>
2015-04-14 20:40:05 -0400

    can create class, which also adds class to user, and redirects user to dashboard

--- classboard/app/controllers/course.server.controller.js
+++ classboard/app/controllers/course.server.controller.js
@@ -7,7 +7,9 @@ var _ = require('lodash'),
 	errorHandler = require('./errors.server.controller'),
 	mongoose = require('mongoose'),
 	passport = require('passport'),
-	Course = mongoose.model('Course');
+	Course = mongoose.model('Course'),
+	//User = require('./users.server.controller');
+	User = mongoose.model('User');
 
 
 
@@ -22,32 +24,64 @@ exports.signUpClass = function(req, res) {
   var randCode = Math.floor(Math.random() * 100000);
 	req.body.code = randCode || 12;
 	
-	var user = req.body.user;
-	delete req.body.user;
 	
-	var course = new Course(req.body);
-	console.log(course);
-	var message = null;
-
-	// Then save the course
-	course.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
+	User.findById(req.body.user._id, function(err, user) {
+		if (!err && user) {
+					
+					// delete user, since we have the real one now
+					delete req.body.user;
+					
+					var course = new Course(req.body)
+					console.log(course);
+					var message = null;
+					
+					// add class to user
+					if(user.classes && user.classes.length > 0) {
+						// user already has some classes
+						user.classes = req.body.user.classes.push(course._id);
+					}
+					else {
+						user.classes = [course._id];
+					}
+					
+					
+					// Then save the course
+					course.save(function(err) {
+						if (err) {
+							return res.status(400).send({
+								message: errorHandler.getErrorMessage(err)
+							});
+						}
+					});
+					
+					
+					user.save(function(err) {
+						if (err) {
+							return res.status(400).send({
+								message: errorHandler.getErrorMessage(err)
+							});
+						} else {
+							req.login(user, function(err) {
+								if (err) {
+									res.status(400).send(err);
+								} else {
+									res.send({
+										message: 'Password changed successfully'
+									});
+								}
+							});
+						}
+					});
+		} else {
+			res.status(400).send({
+				message: 'User is not found'
 			});
 		}
-			
-			// add classes to user's list of classes
-			user.classes = user.classes.push(course._id);
-			
-			// save user
-			user.save(function(err) {
-				if (err) {
-					return res.status(400).send({
-						message: errorHandler.getErrorMessage(err)
-					});
-				}
-			});
-			
 	});
+	
 };
+	
+	
+	
+	
+	
