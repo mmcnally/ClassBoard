84806edabfb68077cc8fb063040f8133545ff1b2
Matt McNally <mgmcnally@umass.edu>
2015-04-16 19:51:25 -0400

    inital commit for settings controller

---
+++ classboard/public/modules/core/controllers/settings.client.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('core').controller('SettingsController', ['$scope', '$http', 'Authentication', '$location', 'Courses',
+	function($scope, $http, Authentication, $location, Courses) {
+		$scope.authentication = Authentication;
+		
+    // send user to signin if not logged in
+		if (!Authentication.user) {
+				$location.path('/signin');
+		}
+		
+	}
+]);
