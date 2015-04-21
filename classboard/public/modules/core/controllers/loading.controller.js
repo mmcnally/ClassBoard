bf036e1d2e4299ab5fc8386f13e5f93d34a0fa69
agora94 <agora@umass.edu>
2015-04-21 19:08:22 -0400

    loadingcontroller now loads current class instead of only first

--- classboard/public/modules/core/controllers/loading.controller.js
+++ classboard/public/modules/core/controllers/loading.controller.js
@@ -7,7 +7,7 @@ angular.module('core').controller('LoadingController', ['$scope', '$http', '$sta
 	    .success(function(response) {
 			Authentication.user.classes = response;
 			console.log(Authentication.user.classes);
-			$state.go('course', {_id : response[0]._id});
+			$state.go('course', {_id : $state.params._id || response[0]._id});
 		})
 		.error(function(response) {
 			console.log(response);
