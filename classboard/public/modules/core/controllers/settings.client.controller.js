e6dfaa5b06a96987b473341fc1b1053e8997b1c7
Matt McNally <mgmcnally@umass.edu>
2015-04-21 19:05:01 -0400

    deleted settings modal from settings controller, since it's in the header controller

--- classboard/public/modules/core/controllers/settings.client.controller.js
+++ classboard/public/modules/core/controllers/settings.client.controller.js
@@ -10,39 +10,7 @@ angular.module('core').controller('SettingsController', ['$scope', '$http', 'Aut
 		}
 		
 		
-		/**
-		* Modal Stuff
-		*/
-
-				// Opens a modal window
-				$scope.open = function (size, currentUser) {
-
-					var modalInstance = $modal.open({
-						templateUrl: 'modules/core/views/settings.client.view.html',
-						controller: function ($scope, $modalInstance, user) {
-							$scope.user = user;
-
-							$scope.ok = function (updatedUser) {
-								console.log(updatedUser);
-								//$scope.user = updatedUser;
-								//console.log(updatedUser.firstName);
-								$modalInstance.close($scope.user);
-							};
-
-							$scope.cancel = function () {
-								$modalInstance.dismiss('cancel');
-							};
-						},
-						size: size,
-						resolve: {
-							user: function () {
-								return currentUser;
-							}
-						}
-					});
-				};
-		
-		
+	
 		
 	}
 ]);
