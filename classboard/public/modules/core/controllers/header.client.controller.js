adcd1483de08d3d1ba7b55923166e72fea95804f
Matt McNally <mgmcnally@umass.edu>
2015-04-21 18:53:33 -0400

    moved settings modal stuff back to where it was (it broke a bit)

--- classboard/public/modules/core/controllers/header.client.controller.js
+++ classboard/public/modules/core/controllers/header.client.controller.js
@@ -22,36 +22,39 @@ angular.module('core').controller('HeaderController', ['$scope', '$http', '$loca
 			return 	$location.url().split('/')[2];
 		};
 
-/**
-* Modal Stuff
-*/
 
-		// Opens a modal window
-		$scope.open = function (size, currentUser) {
 
-			var modalInstance = $modal.open({
-				templateUrl: 'modules/core/views/settings.client.view.html',
-				controller: function ($scope, $modalInstance, user) {
-					$scope.user = user;
+		/**
+		* Modal Stuff
+		*/
 
-					$scope.ok = function (updatedUser) {
-						console.log(updatedUser);
-						//$scope.user = updatedUser;
-						//console.log(updatedUser.firstName);
-						$modalInstance.close($scope.user);
-					};
+				// Opens a modal window
+				$scope.open = function (size, currentUser) {
 
-					$scope.cancel = function () {
-						$modalInstance.dismiss('cancel');
-					};
-				},
-				size: size,
-				resolve: {
-					user: function () {
-						return currentUser;
-					}
-				}
-			});
-		};
+					var modalInstance = $modal.open({
+						templateUrl: 'modules/core/views/settings.client.view.html',
+						controller: function ($scope, $modalInstance, user) {
+							$scope.user = user;
+
+							$scope.ok = function (updatedUser) {
+								console.log(updatedUser);
+								//$scope.user = updatedUser;
+								//console.log(updatedUser.firstName);
+								$modalInstance.close($scope.user);
+							};
+
+							$scope.cancel = function () {
+								$modalInstance.dismiss('cancel');
+							};
+						},
+						size: size,
+						resolve: {
+							user: function () {
+								return currentUser;
+							}
+						}
+					});
+				};
+
 	}
 ]);
