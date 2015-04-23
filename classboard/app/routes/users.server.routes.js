64d26e76a50424244ec7fff8bb69044f4094470d
agora94 <agora@umass.edu>
2015-04-23 16:49:21 -0400

    added structure for filters but not implementing any yet

--- classboard/app/routes/users.server.routes.js
+++ classboard/app/routes/users.server.routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 
 	// Setting up the users profile api
 	app.route('/users/me').get(users.me);
-	app.route('/users').put(users.update);
+	app.route('/users/update').put(users.update);
 
 	// Setting up the users password api
 	app.route('/users/password').post(users.changePassword);
