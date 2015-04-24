4080211201b75fa01db4b846c8c1b581244e16f6
Matt McNally <mgmcnally@umass.edu>
2015-04-23 20:10:12 -0400

    creating new methods

--- classboard/public/modules/widgets/directives/attendance.js
+++ classboard/public/modules/widgets/directives/attendance.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('widgets').directive('attendance', ['$http', '$state', function($http, $state) {
+angular.module('widgets').directive('attendance', ['$http', '$state', 'Authentication', function($http, $state, Authentication) {
 	
 	function link($scope, element, attrs) {
 		$scope.foo = 'hi';
 		$scope.clickedAttend = false;
-		
+		//$scope.newAttendance = {course: Authentication.course._id, students: [], };
 		
 		$scope.attend = function() {
 				$scope.clickedAttend = true;			
+		}
+		
+		$scope.createNewAttendance = function() {
+				
 		}
 		
 		$scope.submit = function() {
