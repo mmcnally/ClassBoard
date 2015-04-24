3f32870e7e2ca956e4e581e74103f2c59a43da94
Matt McNally <mgmcnally@umass.edu>
2015-04-23 20:56:12 -0400

    added beginnings of making attendance widget

--- classboard/public/modules/widgets/directives/attendance.js
+++ classboard/public/modules/widgets/directives/attendance.js
@@ -5,7 +5,10 @@ angular.module('widgets').directive('attendance', ['$http', '$state', 'Authentic
 	function link($scope, element, attrs) {
 		$scope.foo = 'hi';
 		$scope.clickedAttend = false;
-		//$scope.newAttendance = {course: Authentication.course._id, students: [], };
+		$scope.attendanceDate = Date.now;
+		//console.log($scope.attende
+		
+		$scope.newAttendance = {course: Authentication.course._id, students: [], courseTime};
 		
 		$scope.attend = function() {
 				$scope.clickedAttend = true;			
