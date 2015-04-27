2050c708e82c144ee2025d7e09a9015c1bc4aec0
Matt McNally <mgmcnally@umass.edu>
2015-04-27 02:00:48 -0400

    added boolean field current to attendance

--- classboard/app/models/attendance.server.model.js
+++ classboard/app/models/attendance.server.model.js
@@ -25,6 +25,9 @@ var AttendanceSchema = new Schema({
 	duration: { // How long students have to mark themselves present
 		type: Number,
 		default: 300
+	},
+	current: {
+		type: Boolean
 	}
 
 });
