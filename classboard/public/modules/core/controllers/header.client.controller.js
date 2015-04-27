ddce8991e5af51590d6a07c3624b2a4f2cb4700c
Matt McNally <mgmcnally@umass.edu>
2015-04-26 22:36:14 -0400

    fixed some indenting

--- classboard/public/modules/core/controllers/header.client.controller.js
+++ classboard/public/modules/core/controllers/header.client.controller.js
@@ -22,38 +22,38 @@ angular.module('core').controller('HeaderController', ['$scope', '$http', '$loca
 		* Modal Stuff
 		*/
 
-				// Opens a modal window
-				$scope.open = function (size, currentUser) {
+		// Opens a modal window
+		$scope.open = function (size, currentUser) {
 
-					var modalInstance = $modal.open({
-						templateUrl: 'modules/core/views/settings.client.view.html',
-						controller: function ($scope, $modalInstance, user) {
-							$scope.user = user;
-							$scope.SaveAndExit = {};
+			var modalInstance = $modal.open({
+				templateUrl: 'modules/core/views/settings.client.view.html',
+				controller: function ($scope, $modalInstance, user) {
+					$scope.user = user;
+					$scope.SaveAndExit = {};
 
-							$scope.ok = function () {
-								$http.put('/users', $scope.SaveAndExit).success(function(user) {
-									Authentication.user = user;
-									$modalInstance.close(user);
-					      }).error(function(err) {
-										console.log(err);
-					          $scope.SaveAndExit.error = err;
-					      });
-							};
-							
-							
-							$scope.cancel = function () {
-								$modalInstance.dismiss('cancel');
-							};
-						},
-						size: size,
-						resolve: {
-							user: function () {
-								return currentUser;
-							}
-						}
-					});
-				};
+					$scope.ok = function () {
+						$http.put('/users', $scope.SaveAndExit).success(function(user) {
+							Authentication.user = user;
+							$modalInstance.close(user);
+			      }).error(function(err) {
+								console.log(err);
+			          $scope.SaveAndExit.error = err;
+			      });
+					};
+					
+					
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
+				},
+				size: size,
+				resolve: {
+					user: function () {
+						return currentUser;
+					}
+				}
+			});
+		};
 
 	}
 ]);
