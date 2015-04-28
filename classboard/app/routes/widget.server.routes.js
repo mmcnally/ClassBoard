ff477de1aeab4e25897a56c934a761aff006cf46
Matt McNally <mgmcnally@umass.edu>
2015-04-27 23:44:23 -0400

    added server routes for confused widget

--- classboard/app/routes/widget.server.routes.js
+++ classboard/app/routes/widget.server.routes.js
@@ -24,4 +24,10 @@ module.exports = function(app) {
     app.route('/widget/attendance/update').post(users.requiresLogin, attendance.update);
     app.route('/widget/attendance/create').post(users.requiresLogin, course.requiresAuthorization, attendance.create);
     app.route('/widget/attendance/getAttendance').post(users.requiresLogin, attendance.getAttendance);
+    
+    app.route('/widget/confused/create').post(users.requiresLogin, course.requiresAuthorization, confused.create);
+    app.route('/widget/confused/update').post(users.requiresLogin, confused.update);
+    app.route('/widget/confused/reset').post(users.requiresLogin, course.requiresAuthorization, confused.reset);
+    app.route('/widget/confused/getConfused').post(users.requiresLogin, confused.getConfused);
+    
 };
