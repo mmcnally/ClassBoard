76201e62eaff0fc249b96901a65f3a2f8f905566
Matt McNally <mgmcnally@umass.edu>
2015-04-28 02:53:19 -0400

    Confused widget works!

--- classboard/public/modules/widgets/directives/confused.js
+++ classboard/public/modules/widgets/directives/confused.js
@@ -18,13 +18,17 @@ function($http, $state, Authentication, Socket, $timeout) {
 			console.log('retrieved confused object');
 			console.log(confused);
 			$scope.ConfusedModel = confused;
-			$scope.confusedCount = confused.students.length;
+			if(!confused.students) {
+				$scope.confusedCount = 0;
+			}
+			else {
+				$scope.confusedCount = confused.students.length;
+			}
 			
-			if($scope.ConfusedModel && $scope.ConfusedModel.students.indexOf($scope.user._id) !== -1) {
+			if($scope.ConfusedModel && $scope.ConfusedModel.students && $scope.ConfusedModel.students.indexOf($scope.user._id) !== -1) {
 				// user already clicked confused button
 				$scope.isConfused = true;
 			}
-			
 		})
 		.error(function(err) {
 			console.log(err);
@@ -34,18 +38,30 @@ function($http, $state, Authentication, Socket, $timeout) {
 			$scope.create();
 		});
 		
-		
-		
+	
 		
 		
 		
 		// update confused when event received
 		Socket.on('update confused', function() {
+			console.log('received update confused message');
 			$http.post('/widget/confused/getConfused', {courseId: $state.params._id})
 			.success(function(confused) {
 				console.log('retrieved confused object');
+				console.log(confused);
 				$scope.ConfusedModel = confused;
-				$scope.confusedCount = confused.students.length;
+				if(!confused.students) {
+					$scope.confusedCount = 0;
+				}
+				else {
+					$scope.confusedCount = confused.students.length;
+					if(confused.students.indexOf($scope.user._id) !== -1) {
+						$scope.isConfused = true;
+					}
+					else {
+						$scope.isConfused = false;
+					}
+				}
 			})
 			.error(function(err) {
 				console.log(err);
@@ -59,7 +75,9 @@ function($http, $state, Authentication, Socket, $timeout) {
 			$http.post('/widget/confused/create', {course: Authentication.course._id, students: []})
 			.success(function(confused) {
 				console.log('confused object created');
-				$scope.ConfusedModel = confused;
+				//$scope.ConfusedModel = confused;
+				
+				Socket.emit('confused changed');
 			})
 			.error(function(err) {
 				console.log(err);
@@ -68,31 +86,6 @@ function($http, $state, Authentication, Socket, $timeout) {
 		};
 		
 		
-		// $scope.start = function() {
-		// 	var defaultDuration = 30;
-		// 	$scope.started = true;
-		// 	Socket.emit('start attendance');
-		// 	console.log('before starting attendance: ' + $scope.AttendanceModel);
-		// 	
-		// 	// var StartModel = $scope.AttendanceModel;
-		// 	// StartModel.courseId = Authentication.course._id;
-		// 	// $http.post('/widget/attendance/create', StartModel)
-		// 	// .success(function(res) {
-		// 	// 	console.log('start success');
-		// 	// });
-		// 	
-		// 	$scope.presentCount = 0;
-		// 	$timeout($scope.submit, ($scope.AttendanceModel.duration || defaultDuration) * 1000); // submits after duration has passed
-		// 	Socket.on('attend', function(student) {
-		// 		$scope.AttendanceModel.students.push({user: student, present: 1});
-		// 		$scope.presentCount += 1;
-		// 		$scope.students.forEach(function(id, index, object) { // remove ids as students mark themselves present until only absent are left
-		// 			if(id === student) {
-		// 				object.splice(index, 1);
-		// 			}
-		// 		});
-		// 	});
-		// };
 		
 		$scope.addConfused = function() {
 			if($scope.ConfusedModel.students) {
@@ -120,7 +113,7 @@ function($http, $state, Authentication, Socket, $timeout) {
 		
 		$scope.reset = function () {
 			$http.post('/widget/confused/reset', 
-			{course: $state.params._id})
+			{course: Authentication.course._id})
 			.success(function() {
 				console.log('confused objects reset');
 				
@@ -132,25 +125,6 @@ function($http, $state, Authentication, Socket, $timeout) {
 				//$scope.ConfusedError = err;
 			});				
 		};
-		
-		// $scope.submit = function() {
-		// 	console.log(Authentication.course);
-		// 	$scope.students.forEach(function(id) { // mark remaining students absent
-		// 		$scope.AttendanceModel.students.push({user: id, present: 0});
-		// 	});
-		// 	$scope.AttendanceModel.current = false;
-		// 	$scope.AttendanceModel.courseId = Authentication.course._id;
-		// 	var SubmitModel = $scope.AttendanceModel;
-		// 	//SubmitModel.courseId = Authentication.course._id;
-		// 	$http.post('/widget/attendance/create', SubmitModel)
-		// 	.success(function(res) {
-		// 		console.log('success');
-		// 	})
-		// 	.error(function(err) {
-		// 		$scope.AttendanceError = err;
-		// 	});
-		// };
-		
 		
 		
 	}
