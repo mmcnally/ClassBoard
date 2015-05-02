7c428b4875fa7522c8da491d72b496ec7bc32602
Matt McNally <mgmcnally@umass.edu>
2015-05-02 14:54:24 -0400

    cleaned up confused server controller

--- classboard/app/controllers/confused.server.controller.js
+++ classboard/app/controllers/confused.server.controller.js
@@ -13,16 +13,12 @@ Confused = mongoose.model('Confused');
 
 // create new confused object
 exports.create = function(req, res) {
-  console.log(req.body);
-  console.log('HHHHHHIIIIIIIIIIIIII');
   var confused = new Confused(req.body);
   confused.save(function(err, confused) {
     if (err) {
-      console.log('create broke');
       res.status(400).send(err);
     } 
     else {
-      console.log('CONFUSED SAVED');
       res.status(200).send(confused);
     }
   });
@@ -55,56 +51,21 @@ exports.reset = function(req, res) {
     else {
       res.sendStatus(200);
     }
-    
   });
 };
-
-//     exec(function(err, confuseds) {
-//   if(!err && confuseds) {
-//     console.log('confuseds: ' + confuseds);
-//     for (var c in confuseds) {
-//       console.log(c);
-//       c.remove(function (err) {
-//         if(err) {
-//           console.log('REMOVE ERROR');
-//           console.log(err);
-//         }
-//       });
-//     }
-//     console.log('reset all confused objects');
-//     res.sendStatus(200);
-//   }
-//   else if(err) {
-//     console.log('reset error');
-//     res.status(400).send(err);
-//   }
-//   else {
-//     console.log('no confused objects to reset');
-//     res.sendStatus(400);
-//   }
-// });
-
 
 
 
 // retrieves confused object
 exports.getConfused = function(req, res) {
   Confused.findOne({course: req.body.courseId}).exec(function(err, confused) {
-    // if(confused.length !== 1) {
-    //   console.log('number of things!!!! ' + confused.length);
-    //   res.status(400).send('too many thangs');
-    // }
     if(!err && confused) {
-      console.log('FOUND IT YAY');
-      console.log(confused);
       res.json(confused);
     }
     else if(err) {
-      console.log('O NOES');
       res.status(400).send(err);
     }
     else {
-      console.log('CONFUSED TOTALLY NOT HERE :(');
       res.status(400).send({
         message: 'Confused not found'
       });
